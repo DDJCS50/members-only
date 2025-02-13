ab4e7094e4c0f51418bac54b0ef35c23d01ab68a
djns <130430285+DDJCS50@users.noreply.github.com>
2025-02-13 10:56:24 -0800

    Add sign up and error handling
    Added basic sign up form for later use and added
    error handling with validation for the form.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,13 +3,67 @@ const db = require("../db/queries");
 // const CustomNotFoundError = require("../errors/CustomNotFoundError");
 // const asyncHandler = require("express-async-handler");
 
+const alphaErr = "must only contain letters.";
+const passErr = "must be at least 8 characters long, contain at least one of each: lowercase letter, uppercase letter, number, symbol";
+
+const validateFirstNameInput = [body("firstName").trim().isAlpha("en-US", { ignore: " " }).withMessage(`First name ${alphaErr}`)];
+const validateLastNameInput = [body("lastName").trim().isAlpha("en-US", { ignore: " " }).withMessage(`Last name ${alphaErr}`)];
+const validatePassword = [body("password").trim().isStrongPassword().withMessage(`Password ${passErr}`)];
+
 exports.indexPageGet = async (req, res, next) => {
   try {
     res.send("The index page");
   } catch (err) {
-    next(err);
+    return next(err);
   }
 };
+
+exports.signUpGet = async (req, res, next) => {
+  try {
+    res.render("signUp");
+  } catch (err) {
+    console.error(err);
+    return next(err);
+  }
+};
+
+exports.signUpPost = [
+  validateFirstNameInput,
+  validateLastNameInput,
+  validatePassword,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      return res.status(400).render("signUp", {
+        errors: errors.array(),
+      });
+    }
+
+    const { firstName, lastName, username, password, adminStatus } = req.body;
+
+    // TODO check the db to see if the db already has that username
+    // CONST USERNAMESELECTED = db.GETUSERNAMEBYNAME(USERNAME)
+    const usernameSelected = [1];
+    const usernameError = [{ msg: "Username already exists" }];
+
+    if (usernameSelected.length > 0) {
+      return res.status(400).render("signUp", {
+        errors: usernameError,
+      });
+    }
+
+    try {
+      res.send("successfully created");
+      // TODO db.INSERTUSER;
+      // res.redirect("/");
+    } catch (err) {
+      console.error("Error creating user:", err);
+      return next(err);
+    }
+  },
+];
 
 // app.use((req, res, next) => {
 //   res.locals.currentUser = req.user;
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,9 +3,13 @@ const indexController = require("../controllers/indexController");
 const indexRouter = Router();
 
 indexRouter.get("/", indexController.indexPageGet);
+indexRouter.get("/sign-up", indexController.signUpGet);
+
+indexRouter.post("/sign-up", indexController.signUpPost);
 
 indexRouter.use((req, res, next) => {
   console.log("Route does not exist");
+  console.log(JSON.stringify(req.headers));
   res.status(404).send({
     status: 404,
     message: "Route does not exist",
